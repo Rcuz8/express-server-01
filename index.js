ce29b99d78547a2df162d1604d9755c5af920cfa
Ryan Cocuzzo <rcocuzzo8@gmail.com>
2018-04-07 23:48:31 -0400

    fourth

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+var axios = require('axios');
 
 // Let Heroku decide port
 var port = process.env.PORT || 8080;
+
+var BASE_URL = "https://api.binance.com";
+var PRICE_URL = "https://api.binance.com/api/v3/ticker/price";
 
 app.get('/', (request, res) => {
     res.send('Conneccteedd!!')
@@ -14,4 +18,28 @@ app.get('/something', (request, res) => {
     }
 )
 
-app.listen(port, () => console.log('Example app listening on post 3000!'))
+app.get('/binance-info', (req, res) => {
+        
+        let _symbol = req.query.symbol
+        console.log();
+        console.log("Creating Binance Info Request...");
+        console.log("EXCHANGE SYMBOL: " + _symbol);
+        axios.get(PRICE_URL, {
+            params: {
+                symbol: _symbol
+            }
+        }
+   )
+  .then(function (response) {
+    console.log("Sending over data: " + response.data.price);
+        
+    res.send(response.data.price);
+  })
+  .catch(function (error) {
+    console.log(error);
+    res.send("COULD NOT FIND!!");
+  });
+    }       
+)
+
+app.listen(port, () => console.log('Cuzzo Server listening on port '+ port + '!'))
