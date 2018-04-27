9672910ac9a03ac100cb45b1b0c2c937180d936b
Ryan Cocuzzo <rcocuzzo8@gmail.com>
2018-04-26 21:03:44 -0400

    update

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ var parser = new Parser();
 // Let Heroku decide port
 var port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
     function makeStruct(names) {
       var names = names.split(' ');
       var count = names.length;
